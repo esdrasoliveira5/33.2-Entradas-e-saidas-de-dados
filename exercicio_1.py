a8acff4ad68e952da644a9a171e6275755c82eea
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-30 08:49:10 -0300

    refact: Exercicio 1 refeito

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,8 +1,14 @@
 
 def verticalName():
     nome = input("Digite seu nome: ")
-    for letra in nome:
-        print(letra)
+    quantidade_letras = len(nome)
+
+    for numero in range(quantidade_letras, 0, -1):
+        word = ''
+        for index in range(numero):
+            word += nome[index]
+
+        print(word)
 
 
 verticalName()
